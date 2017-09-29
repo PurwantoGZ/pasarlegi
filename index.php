f4d74b011d3d4bac796aecdaa74b36ae58827656
Abdul Rozak <abdul.rozak@ciptakaryateknologi.biz>
2017-09-29 16:19:18 +0700

    Add Index

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Pasar Legi.com</title>
+</head>
+<body>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis natus placeat illo voluptate expedita, voluptas necessitatibus cupiditate doloribus ab, ratione temporibus eos fugiat quisquam perferendis saepe, blanditiis quia, eligendi excepturi.
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quam quos provident assumenda, totam tempora consectetur. Quasi omnis labore veritatis est esse necessitatibus eaque iure expedita, officia illum natus laboriosam in.
+</body>
+</html>
